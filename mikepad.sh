69fd0c180d67273045bf20d04db8cf4247b160cf
admorsus <admorsus3@gmail.com>
2020-05-10 15:25:14 +0200

    Minor changes

---
+++ mikepad.sh
@@ -0,0 +1,2 @@
+#Default eclipse run command for Mikepad 
+/usr/lib/jvm/java-11-openjdk-amd64/bin/java -Dfile.encoding=UTF-8 -classpath /home/mbp/eclipse/Mikepad/target/classes:/home/mbp/.m2/repository/org/drjekyll/fontchooser/2.4/fontchooser-2.4.jar:/home/mbp/.m2/repository/com/fifesoft/rsyntaxtextarea/3.1.1/rsyntaxtextarea-3.1.1.jar mikepad.Mikepad
